937ed434a594bd92faf96b7f71505dd013d1a11e
Matthew-Foley-Student <foleymat000@gmail.com>
2025-10-18 00:53:26 -0700

    Add files via upload

---
+++ MineSweeperLibrary-Week3/Models/BoardModel.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Matthew Foley
+ * CST-250
+ * Augest 2025
+ * Final Project
+ */
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeperLibrary.Models
+{
+    public class BoardModel
+    {
+        public int Size { get; private set; }
+        public CellModel[,] Grid { get; set; }
+
+        public BoardModel(int size)
+        {
+            Size = size;
+            Grid = new CellModel[Size, Size];
+            InitilzeBoard();
+        }
+        private void InitilzeBoard()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Grid[i, j] = new CellModel(i, j);
+                }
+            }
+        }
+    }
+}
---
+++ MineSweeperLibrary-Week3/Services/Buisness Logic/BoardLogic.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Matthew Foley
+ * CST-250
+ * 10/6/2025
+ * Activity 2
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MineSweeperLibrary.Models;
+
+namespace ChessBoardLibrary.Services.BuisnessLogic
+{
+    public class BoardLogic
+    {
+        private BoardModel ResetBoard(BoardModel board)
+        {
+            foreach (CellModel cell in board.Grid)
+            {
+                cell.Bomb = false;
+                cell.Flag = false;
+                cell.Revealed = false;
+                cell.BombRefrence = 0;
+            }
+            return board;
+        }
+
+        public BoardModel IsOnBoard(BoardModel board, CellModel currentCell)
+        {
+            board.Grid[currentCell.Row, currentCell.Collumn].Revealed = true;
+            return board;
+        }
+
+        public BoardModel CheckForBomb (BoardModel board, CellModel currentCell, bool bombRefrence)
+        {
+            if (board.Grid[currentCell.Row,currentCell.Collumn].Bomb == true)
+            {
+
+            }
+            else
+            {
+                board.Grid[currentCell.Row, currentCell.Collumn].Revealed = true;
+            }
+            return board;
+        }
+
+    }
+}
+
+        
